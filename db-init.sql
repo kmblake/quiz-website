7178080fb4052964b17c5d913c4db0ff728f3741
kmblake <kentblake@dn0a224390.sunet>
2014-11-08 12:19:03 -0800

    Added email field

---
+++ db-init.sql
@@ -0,0 +1,14 @@
+USE c_cs108_kmblake;
+
+-- Create user table
+
+DROP TABLE IF EXISTS users;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE users (
+    id INTEGER,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255),
+    email VARCHAR(255)
+);
