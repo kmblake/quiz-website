dd477cf3c68fdf36c3fb4ee209d63d2713119ea0
bryantc99 <bachen@stanford.edu>
2014-11-09 23:54:27 -0800

    Helios
    Helios

---
+++ Quiz Website/src/quiz/AccountManager.java
@@ -0,0 +1,34 @@
+package quiz;
+
+import java.sql.*;
+
+public class AccountManager {
+	
+	DBConnection database;
+	Statement stmt;
+	
+	public AccountManager(DBConnection database) {
+		this.database = database;
+		this.stmt = database.getStatement();
+
+	}
+	
+	public boolean accountExists(String user) {
+			//TODO
+		return false;
+	}
+	
+	public boolean passwordMatches(String user, String pass) {
+		if (accountExists(user)) {
+			//TODO
+			//return (database.get(user).equals(pass));
+			return false;
+		}
+		return false;
+	}
+	
+	public void createAccount(String user, String pass) {
+		//database.put(user, pass);
+		//TODO
+	}
+}
---
+++ Quiz Website/src/quiz/ContextInitializer.java
@@ -0,0 +1,41 @@
+package quiz;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class DBListener
+ *
+ */
+@WebListener
+public class ContextInitializer implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextInitializer() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+    	DBConnection connection = new DBConnection();
+        ServletContext context = arg0.getServletContext();
+        context.setAttribute("connection", connection);
+        context.setAttribute("statement", connection.getStatement());
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+        ServletContext context = arg0.getServletContext();
+        DBConnection connection = (DBConnection)context.getAttribute("connection");
+        connection.close();
+    }
+	
+}
